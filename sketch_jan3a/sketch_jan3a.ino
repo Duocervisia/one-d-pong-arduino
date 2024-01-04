90797217889f7d05e2a40e0cd9de03ec28a03760
Tobias Henning <tobias081996@googlemail.com>
2024-01-04 20:24:27 +0100

    Few fixes

--- sketch_jan3a/sketch_jan3a.ino
+++ sketch_jan3a/sketch_jan3a.ino
@@ -1,13 +1,13 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 #include <ezButton.h>
 
-#define PIN 13
-#define NUMPIXELS 100
-#define GAME_PIXEL_WIDTH 10
+#define DATA_PIN 5
+#define BRIGHTNESS 5
+#define NUM_LEDS 100
+#define GAME_LED_WIDTH 10
 #define ONULL -1
 
-
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[NUM_LEDS];
 
 ezButton playerOneButton(14);
 ezButton playerTwoButton(12);
@@ -19,8 +19,8 @@ bool gameRunning;
 
 unsigned long lastUpdateTime;
 int gameDelay;
-int minDelay = 20;
-int maxDelay = 100;
+int minDelay = 10;
+int maxDelay = 2000;
 
 void setup() {
   Serial.begin(115200);
@@ -29,9 +29,10 @@ void setup() {
   playerOneButton.setDebounceTime(50);
   playerTwoButton.setDebounceTime(50);
 
-  pixels.begin();
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+
   setupGame();
-  pixels.show();
 }
 
 void loop() {
@@ -40,29 +41,22 @@ void loop() {
 
   unsigned long currentTime = millis();
   
-  if(playerOneButton.isPressed()){
+  if(playerOneButton.isReleased()){
+    Serial.println("press player 1");
     if(!gameRunning){
       startGame(1);
-      updateBallVisual();
     }else if(ballDirection == -1){
       shootBack(1);
-      if(updateBallPosition()){
-        updateBallVisual();
-      }
     }
-  }else if(playerTwoButton.isPressed()){
+  }
+  if(playerTwoButton.isReleased()){
     if(!gameRunning){
       startGame(2);
-      updateBallVisual();
     }else if(ballDirection == 1){
       shootBack(2);
-      if(updateBallPosition()){
-        updateBallVisual();
-      }
     }
-  }else if (gameRunning && currentTime - lastUpdateTime >= gameDelay) {
-    //Serial.println(currentTime - lastUpdateTime);
-  	//Serial.println(gameDelay);
+  }
+  if (gameRunning && currentTime - lastUpdateTime >= gameDelay) {
     if(updateBallPosition()){
       updateBallVisual();
     }
@@ -70,62 +64,72 @@ void loop() {
   }
 }
 
-
 void setupGame() {
-  for (int i = 0; i < GAME_PIXEL_WIDTH; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
-    pixels.setPixelColor(NUMPIXELS - 1 - i, pixels.Color(0, 255, 0));
+  for (int i = 0; i < GAME_LED_WIDTH; i++) {
+    leds[i] = CRGB(0, 255, 0);
+    leds[NUM_LEDS - 1 - i] = CRGB(0, 255, 0);
+    FastLED.show();
   }
 }
 
 void startGame(int player) {
   if (player == 1) {
-    ballPosition = GAME_PIXEL_WIDTH;
+    ballPosition = GAME_LED_WIDTH - 1;
     ballDirection = 1;
   } else {
-    ballPosition = NUMPIXELS - 1 - GAME_PIXEL_WIDTH;
+    ballPosition = NUM_LEDS - GAME_LED_WIDTH;
     ballDirection = -1;
   }
   gameDelay = (maxDelay - minDelay) / 2;
+  lastUpdateTime = gameDelay;
+  Serial.println(gameDelay);
   gameRunning = true;
 }
-void shootBack(int player) {
+
+bool shootBack(int player) {
   if(
-  	(player == 1 && ballPosition < GAME_PIXEL_WIDTH) ||
-    (player == 2 && ballPosition >= NUMPIXELS - GAME_PIXEL_WIDTH)
+    (player == 1 && ballPosition < GAME_LED_WIDTH) ||
+    (player == 2 && ballPosition >= NUM_LEDS - GAME_LED_WIDTH)
   ){
     ballDirection *= -1;
     
-    if(ballPosition < GAME_PIXEL_WIDTH){
-      gameDelay = minDelay + (ballPosition/(GAME_PIXEL_WIDTH-1)) * (maxDelay - minDelay);
+    if(ballPosition < GAME_LED_WIDTH){
+      gameDelay = minDelay + (float(ballPosition)/ float(GAME_LED_WIDTH-1)) * (maxDelay - minDelay);
     }else{
-      gameDelay = minDelay + ((NUMPIXELS - 1 - ballPosition)/ (GAME_PIXEL_WIDTH-1)) * (maxDelay - minDelay);
+      gameDelay = minDelay + (float(NUM_LEDS - 1 - ballPosition) / float(GAME_LED_WIDTH-1)) * (maxDelay - minDelay);
     }
+    Serial.println(String(minDelay) + " + " +  String(ballPosition) + " / " + String(GAME_LED_WIDTH-1) + " * " + String(maxDelay - minDelay) + " = " + String(gameDelay));
+
   }else{
-  	endGame();
+    endGame();
+    return false;
   }
+  return true;
 }
+
 void endGame(){
   gameRunning = false;
   prevBallPosition = ONULL;
-  pixels.setPixelColor(ballPosition, pixels.Color(0, 0, 0));
+  leds[ballPosition] = CRGB(0, 0, 0);
   for(int j = 0; j <= 6; j++){
-    pixels.setPixelColor(0, pixels.Color(j%2 == 0 ? 255 : 0, 0, 0));
-    pixels.setPixelColor(NUMPIXELS - 1, pixels.Color(j%2 == 0 ? 255 : 0, 0, 0));
+    leds[0] = CRGB(j%2 == 0 ? 255 : 0, 0, 0);
+    leds[NUM_LEDS - 1] = CRGB(j%2 == 0 ? 255 : 0, 0, 0);
 
-  	for (int i = j==6 ? 0 : 1; i < GAME_PIXEL_WIDTH; i++) {
-      pixels.setPixelColor(i, pixels.Color(0, j%2 == 0 ? 255 : 0, 0));
-      pixels.setPixelColor(NUMPIXELS - 1 - i, pixels.Color(0, j%2 == 0 ? 255 : 0, 0));
-      pixels.show();
-      delay(100);
-  	}
+    for (int i = j==6 ? 0 : 1; i < GAME_LED_WIDTH; i++) {
+      leds[i] = CRGB(0, j%2 == 0 ? 255 : 0, 0);
+      leds[NUM_LEDS - 1 - i] = CRGB(0, j%2 == 0 ? 255 : 0, 0);
+      FastLED.show();
+      delay(20);
+    }
   }
+  playerOneButton.loop();
+  playerTwoButton.loop();
 }
 
 bool updateBallPosition() {
   prevBallPosition = ballPosition;
   ballPosition += ballDirection;
-  if(ballPosition < 0 || ballPosition > NUMPIXELS - 1){
+  if(ballPosition < 0 || ballPosition > NUM_LEDS - 1){
     endGame();
     return false;
   }
@@ -133,15 +137,15 @@ bool updateBallPosition() {
 }
 
 void updateBallVisual() {
-  pixels.setPixelColor(ballPosition, pixels.Color(0, 0, 255));
-  Serial.println(ballPosition);
+  leds[ballPosition] = CRGB(0, 0, 255);
+  // Serial.println(ballPosition);
 
   if (prevBallPosition != ONULL){
-    if((prevBallPosition < GAME_PIXEL_WIDTH && ballDirection == 1) ||(prevBallPosition >= NUMPIXELS - GAME_PIXEL_WIDTH && ballDirection == -1)){
-      pixels.setPixelColor(prevBallPosition, pixels.Color(0, 255, 0));
+    if((prevBallPosition < GAME_LED_WIDTH && ballDirection == 1) ||(prevBallPosition >= NUM_LEDS - GAME_LED_WIDTH && ballDirection == -1)){
+      leds[prevBallPosition] = CRGB(0, 255, 0);
     }else{
-      pixels.setPixelColor(prevBallPosition, pixels.Color(0, 0, 0));
-	}
+      leds[prevBallPosition] = CRGB(0, 0, 0);
+    }
   }
-  pixels.show();
+  FastLED.show();
 }
